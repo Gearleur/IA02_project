1016c99c92798bb5657029b18675ba0490c2639e
marion <marion.forestier@etu.utc.fr>
2024-06-17 17:04:22 +0200

    bug test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from gopher import *
 
     
-def main_alpha_gopher():
+#def main_alpha_gopher():
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Initialisation du jeu
