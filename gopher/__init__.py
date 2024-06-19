20f008badc6ddbd9afa797ed9b95f0580c780e03
Teixeira Alexandre <alexandretei13@gmail.com>
2024-06-19 10:57:32 +0200

    up

--- gopher/__init__.py
+++ gopher/__init__.py
@@ -2,4 +2,4 @@
 from .hex import Hex, Point, hex_neighbor, hex_add, hex_subtract, idx_to_hex, encoded_to_server
 from .game_logic import GopherGame
 from.mcts_alpha import ResNet, ResBlock, MCTSAlpha, NodeAlpha, AlphaZero, AlphaZeroParallel, MCTSAlphaParallel, MockResNet
-from .mcts_alpha_gpu import ResNetGPU, ResBlockGPU, MCTSAlphaGPU, NodeAlphaGPU, AlphaZeroGPU, AlphaZeroParallelGPU, MCTSAlphaParallelGPU
+from .mcts_alpha_gpu import ResNetGPU, ResBlockGPU, NodeAlphaGPU, AlphaZeroParallelGPU, MCTSAlphaParallelGPU
