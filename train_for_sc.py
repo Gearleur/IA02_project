d64ec1c779ef0379a311b1b9b8e78c1b858d0ae7
Teixeira Alexandre <alexandretei13@gmail.com>
2024-06-20 16:15:43 +0200

    train up

--- train_for_sc.py
+++ train_for_sc.py
@@ -8,7 +8,7 @@ from gopher import *
 # Spécifiez les GPUs à utiliser
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2"  # Utiliser les GPUs 0, 1 et 2
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2"  # Utiliser les GPUs 0, 1 et 2
 
 gopher = GopherGame()
 
@@ -17,22 +17,22 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = ResNetGPU(gopher, num_resBlocks=9, num_hidden=128, device=device)
 
 # Utiliser DataParallel pour utiliser plusieurs GPUs
-if torch.cuda.device_count() > 1:
-    print(f"Utilisation de {torch.cuda.device_count()} GPUs")
-    model = nn.DataParallel(model)
+# if torch.cuda.device_count() > 1:
+#     print(f"Utilisation de {torch.cuda.device_count()} GPUs")
+#     model = nn.DataParallel(model)
 
 model.to(device)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-4)
 
 args = {
     "num_searches": 800,
     "C": 2,
     "num_iterations": 7,
     "num_selfPlay_iterations": 500,
-    "num_parallel_games": 50,
+    "num_parallel_games": 100,
     "num_epochs": 4,
-    "batch_size": 128,
+    "batch_size": 32,
     "temperature": 1,
     "dirichlet_epsilon": 0.25,
     "dirichlet_alpha": 0.3,
